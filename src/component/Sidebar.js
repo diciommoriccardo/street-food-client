53f7aef79e152deae13246969db5b1284661ea91
nucasu <casucci.nunzio1@gmail.com>
2022-06-15 16:22:21 +0200

    sidebarjs

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
     BrowserRouter as Router, 
     NavLink } from 'react-router-dom';
+import "../styles/sidebar.css";
 
 class Sidebar extends React.Component{
     constructor(){
@@ -13,9 +14,9 @@ class Sidebar extends React.Component{
 
     render() {
         return( 
-            <div>
-               <ul>
-                    <li><NavLink to="/"> Menù</NavLink></li>
+            <div className="sidebarContainer">
+               <ul className="listaContainer">
+                    <li className="active"><NavLink to="/"> Menu</NavLink></li>
                     <li><NavLink to="/orders">Ordini</NavLink></li>
                     <li><NavLink to="/cart">Carrello</NavLink></li>
                 </ul> 

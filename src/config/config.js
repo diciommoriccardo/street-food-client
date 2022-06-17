37dcd573b1e00ee9ff9919e2d51dfa8d6eeb0765
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-06-17 16:31:10 +0200

    config

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+const SERVER_REST = {
+    HOST: '0.0.0.0',
+    PORT:  8080,
+    HOST_SECURE: '0.0.0.0',
+    PORT_SECURE: 8443
+}
+
+const SERVER_SOCKET = {
+    HOST: '0.0.0.0',
+    PORT: 3000
+}
+
+export {
+    SERVER_REST,
+    SERVER_SOCKET
+}

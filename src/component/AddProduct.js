70c3b3f361588808a41fa954e8e7ed3879ff3895
nucasu <casucci.nunzio1@gmail.com>
2022-06-29 10:34:10 +0200

    orders

--- src/component/AddProduct.js
+++ src/component/AddProduct.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import '../styles/aggiuntaProdotto.css'
+// import {DropzoneArea} from 'material-ui-dropzone'
 
 const currencies = [
   {
@@ -39,18 +40,18 @@ export default function AddProduct(props) {
         '& .MuiTextField-root': { m: 1, width: '25ch' },
       }}
       noValidate
-      autoComplete="off"
+      autoComplete="on"
     >
      <div className='inner'>
-      <div className='AggiuntaProdotto'>
+      <div className='AggiuntaProdottocontainer'>
       <button className="ButtonClose" onClick={()=>props.setTrigger(false) }>Chiudi aggiunta Prodotto</button>
-      <TextField className='AggiuntaProdotto' id="Nome prodotto" label="Nome" variant="filled" />
-      <TextField className='AggiuntaProdotto' id="Ricetta" label="Ricetta" variant="filled" />
-      <TextField className='AggiuntaProdotto' id="Descrizione" label="Breve descrizione" variant="filled" />
-      <TextField className='AggiuntaProdotto' id="Linkimmagine" label="Link immagine" variant="filled" /><p>Carica l' immagine tramite ftp nella cartella FotoProdotti</p>
+      <TextField className='AggiuntaProdotto' id="Nome prodotto" label="Nome" variant="outlined" />
+      <TextField className='AggiuntaProdotto' id="Ricetta" label="Ricetta" variant="outlined" />
+      <TextField className='AggiuntaProdotto' id="Descrizione" label="Breve descrizione" variant="outlined" />
+      <DropzoneArea> </DropzoneArea>
 
         <TextField
-          className='AggiuntaProdotto'
+          className='AggiuntaProdottoCategoria'
           id="Categoria"
           select
           label="Seleziona Categoria"
@@ -60,7 +61,7 @@ export default function AddProduct(props) {
             native: true,
           }}
           helperText="Seleziona la categoria"
-          variant="filled"
+          variant="outlined"
         >
           {currencies.map((option) => (
             <option key={option.value} value={option.value}>
